c086c4fe1dda742cc8973b9c58a465d66fd3c906
Gerry1135 <gerry@madeupplace.com>
2017-04-17 17:21:05 +0100

    Updated version to 1.1.3.0 specifically for KSP 1.2.9

--- KerbalEngineer/EngineerGlobals.cs
+++ KerbalEngineer/EngineerGlobals.cs
@@ -25,7 +25,7 @@ namespace KerbalEngineer
         /// <summary>
         ///     Current version of the Kerbal Engineer assembly.
         /// </summary>
-        public const string ASSEMBLY_VERSION = "1.1.2.8";
+        public const string ASSEMBLY_VERSION = "1.1.3.0";
 
         private static string assemblyFile;
         private static string assemblyName;
